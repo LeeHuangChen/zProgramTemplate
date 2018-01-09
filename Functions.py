2a49105e5c5c74f8fec4a0d72b6cb427cb082f3d
LeeHuangChen <lhc1@rice.edu>
2018-01-09 11:13:43 -0600

    added for all input file operation

--- Functions.py
+++ Functions.py
@@ -1,5 +1,16 @@
+import os
+import Configuration as conf
+import util
+
+
+def forAllInputFiles():
+	inputfiles=os.listdir(conf.inputFolder)
+
+	for i, inputfile in enumerate(inputfiles):
+		pass
+
 def main():
-	print 0
+	pass
 
 if __name__ == '__main__':
 	main()
