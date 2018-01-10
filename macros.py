c0f9071e08e9f26268c9884511663c3235e3c546
Lee <lhc1@rice.edu>
2018-01-10 13:36:51 -0600

    updated the tempate

--- macros.py
+++ macros.py
@@ -4,15 +4,16 @@ def forAllLineInFile(fileInfo, doworkwithline):
 	#fileInfo=(fileIndex, inputFolder, inputfile)
 	inputFolder=fileInfo[1]
 	inputfile=fileInfo[2]
-	lineIndex=0
-	with open(os.path.join(inputFolder,inputfile),"r") as f:
-		for line in iter(f):
-			lineInfo=(lineIndex,line)
+	
+	filepath=os.path.join(inputFolder,inputfile)
+	with open(filepath,"r") as f:
+		for i, line in enumerate(f):
+			lineInfo=(i,line)
 			doworkwithline(fileInfo,lineInfo)
-			lineIndex+=1
+			
 
 
-def forAllInputFiles(doworkwithfile,folder):
+def forAllFiles(doworkwithfile,folder):
 	inputfiles=os.listdir(folder)
 
 	for i, inputfile in enumerate(inputfiles):
